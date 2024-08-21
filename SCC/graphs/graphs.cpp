ec942b01e8692254b593f296760a4584ff73467a
lok70 <lokwer321@gmail.com>
2024-08-21 16:32:26 +0300

    add

---
+++ SCC/graphs/graphs.cpp
@@ -0,0 +1,18 @@
+﻿#include "iostream";
+#include "Header.h"
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    Graph g(5);
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 1);
+    g.addEdge(0, 3);
+    g.addEdge(3, 4);
+
+    cout << "Сильно связанные компоненты:\n";
+    g.SCC();
+    g.printSCCs();
+
+    return 0;
+}
